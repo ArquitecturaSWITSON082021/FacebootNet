978e1b5db7a2b2fd52cf0d21a43fcbd39e47cc5f
Brayan Iribe <brayaniribe@gmail.com>
2021-10-20 20:59:36 -0700

    Initial commit

---
+++ src/FacebootNet/Constants.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FacebootNet;
+
+/**
+ *
+ * @author Ivy
+ */
+public class Constants {
+    public static final short packetHeader = 9281;
+    public static final int packetLength = 1024 * 4;
+}
---
+++ src/FacebootNet/Packets/Opcodes.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package FacebootNet.Packets;
+
+/**
+ *
+ * @author Ivy
+ */
+public class Opcodes {
+    public static final int HelloNfy = 1;
+}
