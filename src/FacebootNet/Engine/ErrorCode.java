3a39e4fea35f6b9acdf5cca386d65f09f358be02
Brayan Iribe <brayaniribe@gmail.com>
2021-11-30 00:33:29 -0700

    Agregado formateador de codigos de erores a la biblioteca de red

--- src/FacebootNet/Engine/ErrorCode.java
+++ src/FacebootNet/Engine/ErrorCode.java
@@ -16,4 +16,23 @@ public class ErrorCode {
     public static final int InternalServerError = -2;
     public static final int InvalidCredentials = 1;
     public static final int UserIllegalAge = 2;
+    
+    public static String Format(int ErrorCode){
+        switch (ErrorCode){
+            case NoError:
+                return "No error.";
+            case SocketError:
+                return "No se ha podido completar la conexión al servidor, SocketError.";
+            case InvalidRequest:
+                return "Se ha recibido una petición inválida.";
+            case InternalServerError:
+                return "Ha ocurrido un error interno en el servidor.";
+            case InvalidCredentials:
+                return "Las credenciales proporcionadas son inválidas.";
+            case UserIllegalAge:
+                return "La edad del usuario es inválida, debe ser mayor de 18 años para hacer uso de Faceboot.";
+        }
+        
+        return String.format("Invalid error code given: %d.", ErrorCode);
+    }
 }
